cdef3b2f455658deb9092831a79bd5fff88142ec
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-11 16:46:45 +0100

    First version of the sidebar without links working

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -5,20 +5,20 @@ import styles from '../styles/components/sidebar.less';
 const Sidebar = ({ styleId }) => (
   <div className={styles.sidebar + ' ' + styleId}>
     <section className={styles.sidebarHeader}>
-      Expensation
+      <span>Expens</span><span>track</span>
     </section>
     <nav className={styles.sidebarNavigation}>
-      <ul>
-        <li>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
           <a href="javascript:;"><i className="fa fa-fw fa-bar-chart" /> Analytics</a>
         </li>
-        <li>
+        <li className={styles.navItem}>
           <a href="javascript:;"><i className="fa fa-fw fa-credit-card" /> Expenses</a>
         </li>
-        <li>
+        <li className={styles.navItem}>
           <a href="javascript:;"><i className="fa fa-fw fa-users" /> Users</a>
         </li>
-        <li>
+        <li className={styles.navItem}>
           <a href="javascript:;"><i className="fa fa-fw fa-cubes" /> Categories</a>
         </li>
       </ul>
