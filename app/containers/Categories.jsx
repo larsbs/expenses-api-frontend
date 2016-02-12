f2ed5db9610e3700f2586fdeaf4ff70449a70681
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-12 14:03:07 +0100

    First version of the static web

--- app/containers/Categories.jsx
+++ app/containers/Categories.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import DataFilter from '../components/DataFilter';
+import DataTable from '../components/DataTable';
+
+import styles from '../styles/containers/expenses.less';
 import headerStyles from '../styles/components/header.less';
 
 
@@ -14,6 +18,10 @@ const Categories = () => (
     <Header breadcrumbs={breadcrumbs}>
       <button className={headerStyles.button}><i className="fa fa-fw fa-plus" /> Add Category</button>
     </Header>
+    <div className={styles.container}>
+      <DataFilter />
+      <DataTable />
+    </div>
   </main>
 );
 
--- app/containers/Users.jsx
+++ app/containers/Users.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import DataFilter from '../components/DataFilter';
+import DataTable from '../components/DataTable';
+
+import styles from '../styles/containers/users.less';
 import headerStyles from '../styles/components/header.less';
 
 
@@ -14,6 +18,10 @@ const Users = () => (
     <Header breadcrumbs={breadcrumbs}>
       <button className={headerStyles.button}><i className="fa fa-fw fa-plus" /> Add User</button>
     </Header>
+    <div className={styles.container}>
+      <DataFilter />
+      <DataTable />
+    </div>
   </main>
 );
 
