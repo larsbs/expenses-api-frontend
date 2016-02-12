6f0919d13da99cdbb2586095bfd9bd91868ba990
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-12 19:09:22 +0100

    Small refactorization of loading

--- app/containers/Application.jsx
+++ app/containers/Application.jsx
@@ -8,34 +8,33 @@ import styles from '../styles/containers/application.less';
 
 
 
+const Loading = () => (
+  <div className={styles.loading}>
+    <Spinner width="100" height="100" borderRadius="15" />
+    <h2 className={styles.loadingTitle}>
+      Wait a moment till I prepare everything for you...
+    </h2>
+  </div>
+);
+
 class Application extends React.Component {
 
   render() {
     const { children, isLoading } = this.props;
 
-    let app;
-    if (true) {
-      app = (
-        <div className={styles.application}>
-          <div className={styles.loading}>
-            <Spinner width="100" height="100" borderRadius="15" />
-            <h2 className={styles.loadingTitle}>
-              Wait a moment, the app is loading...
-            </h2>
-          </div>
-        </div>
-      );
-    }
-    else {
-      app = (
-        <div className={styles.application}>
-          <Sidebar styleId="sidebar" />
-          {children}
-        </div>
-      );
-    }
+    const loading = (
+      <div className={styles.application}>
+        <Loading />
+      </div>
+    );
+    const app = (
+      <div className={styles.application}>
+        <Sidebar styleId="sidebar" />
+        {children}
+      </div>
+    );
 
-    return app;
+    return isLoading ? loading : app;
   }
 
 }
