15d4804c9025036782ef1f76485c4f8ae34bb6ea
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-12 23:56:56 +0100

    DataTable pagination working

--- app/components/DataTable.jsx
+++ app/components/DataTable.jsx
@@ -23,6 +23,10 @@ class DataTable extends React.Component {
     const { entries } = this.props;
   }
 
+  componentDidUpdate() {
+    this._totalPages = Math.ceil(this.props.entries.length / this.state.entriesPerPage);
+  }
+
   render() {
     const { columns, entries } = this.props;
     const { firstShownEntry, lastShownEntry, shownEntries } = this._getShownEntries();
@@ -68,13 +72,13 @@ class DataTable extends React.Component {
         </table>
         <div className={styles.tablePagination}>
           <ul className={styles.tablePaginationControl}>
-            <li>
+            <li onClick={this._handleOnClickPrev.bind(this)}>
               <i className="fa fa-caret-left fa-fw" /> Prev
             </li>
             <li>
-              <input type="text" defaultValue={this.state.currentPage} />
+              <input type="text" value={this.state.currentPage} onChange={this._handleOnChangePage.bind(this)}/>
             </li>
-            <li>
+            <li onClick={this._handleOnClickNext.bind(this)}>
               Next <i className="fa fa-caret-right fa-fw" />
             </li>
           </ul>
@@ -85,15 +89,21 @@ class DataTable extends React.Component {
 
 
   _getShownEntries() {
+    let { currentPage } = this.state;
+
+    if ( ! isValidPage(currentPage) || currentPage > this._totalPages) {
+      currentPage = 1;
+    }
+
     const firstShownEntry = ifNaNDefault(
       Math.min(
         this.props.entries.length,
-        this.state.entriesPerPage * this.state.currentPage - this.state.entriesPerPage + 1
+        this.state.entriesPerPage * currentPage - this.state.entriesPerPage + 1
       ), 0
     );
     const lastShownEntry = ifNaNDefault(
       Math.min(
-        this.state.entriesPerPage * this.state.currentPage,
+        this.state.entriesPerPage * currentPage,
         this.props.entries.length
       ), 0
     );
@@ -109,16 +119,20 @@ class DataTable extends React.Component {
     this.setState({ entriesPerPage: event.target.value });
   }
 
-  _handleOnClickNext() {
+  _handleOnClickPrev() {
     if (this.state.currentPage > 1) {
       this.setState({ currentPage: this.state.currentPage - 1 });
     }
   }
 
-  _handleOnClickPrev() {
+  _handleOnClickNext() {
     if (this.state.currentPage < this._totalPages) {
-
+      this.setState({ currentPage: this.state.currentPage + 1 });
     }
+  }
+
+  _handleOnChangePage(event) {
+    this.setState({ currentPage: event.target.value });
   }
 
 }
