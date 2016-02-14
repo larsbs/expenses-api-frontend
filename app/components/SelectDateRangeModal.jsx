a0460f7d69b73b457881978f4d7d7e44419e3a14
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-14 22:45:15 +0100

    First version that appears to work of the datepickers

--- app/components/SelectDateRangeModal.jsx
+++ app/components/SelectDateRangeModal.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import jQuery from 'jquery';
 import Modal from 'react-modal';
-import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import DatePicker from 'react-date-picker';
+import DateField from 'react-date-field';
 
-import 'react-datepicker/dist/react-datepicker.css';
+import 'react-date-picker/base.css';
+import 'react-date-picker/theme/hackerone.css';
 import formStyles from '../styles/forms.less';
 import modalStyles from '../styles/modal.less';
 
@@ -22,7 +26,8 @@ const defaultModalStyle = {
     top: 'auto',
     right: 'auto',
     bottom: 'auto',
-    left: 'auto'
+    left: 'auto',
+    overflow: 'show'
   }
 };
 
@@ -31,69 +36,96 @@ class SelectDateRangeModal extends React.Component {
   state = {
     hasError: false,
     errorMsg: '',
-    dateTo: this.props.initialDateTo,
-    dateFrom: this.props.initialDateFrom
+    dateTo: moment(this.props.initialDateTo),
+    dateFrom: moment(this.props.initialDateFrom)
   };
 
   render() {
     const { isModalOpen, onCloseModal } = this.props;
+    const dateTo = moment(this.state.dateTo);
+    const dateFrom = moment(this.state.dateFrom);
 
     return (
-      <Modal isOpen={isModalOpen} onRequestClose={onCloseModal} style={defaultModalStyle}>
-        <div className={modalStyles.modal}>
-          <div className={modalStyles.header}>
-            <div className={modalStyles.title}>Select Date Range</div>
-            <i className={modalStyles.close + ' fa fa-fw fa-times'} onClick={onCloseModal} />
-          </div>
-          <div className={modalStyles.body}>
-            <form onSubmit={this._handleOnClickSubmit.bind(this)}>
-              <div className={formStyles.formGroup}>
-                <label className={formStyles.formLabel}>From</label>
-                <div className={formStyles.inputFieldWithSymbol}>
-                  <DatePicker
-                    className={formStyles.inputField}
-                    selected={this.state.dateFrom}
-                    maxDate={this.state.dateTo.clone().subtract(1, 'day')}
-                    onChange={this._handleOnChangeDateFrom.bind(this)}
-                    placeholderText="Click here to select a date" />
-                  <div className={formStyles.inputFieldSymbol}>
-                    <i className="fa fa-fw fa-calendar" />
+      <div>
+        <Modal isOpen={isModalOpen} onRequestClose={onCloseModal} style={defaultModalStyle}>
+          <div className={modalStyles.modal}>
+            <div className={modalStyles.header}>
+              <div className={modalStyles.title}>Select Date Range</div>
+              <i className={modalStyles.close + ' fa fa-fw fa-times'} onClick={onCloseModal} />
+            </div>
+            <div className={modalStyles.body}>
+              <form onSubmit={this._handleOnClickSubmit.bind(this)}>
+                <div className={formStyles.formGroup}>
+                  <label className={formStyles.formLabel}>From</label>
+                  <div className={formStyles.inputFieldWithSymbol}>
+                    <DateField
+                      ref="inputDateFrom"
+                      clearIcon={false}
+                      renderInput={props => {
+                        props.className = formStyles.inputField;
+                      }}>
+                      <DatePicker
+                        date={dateFrom}
+                        maxDate={dateTo.clone().subtract(1, 'day')}
+                        onChange={this._handleOnChangeDateFrom.bind(this)}
+                        hideFooter={true}/>
+                    </DateField>
+                    <div className={formStyles.inputFieldSymbol}>
+                      <i className="fa fa-fw fa-calendar" />
+                    </div>
                   </div>
+                  <span className={formStyles.errorMessage}>{this.state.errorMsg}</span>
                 </div>
-                <span className={formStyles.errorMessage}>{this.state.errorMsg}</span>
-              </div>
-              <div className={formStyles.formGroup}>
-                <label className={formStyles.formLabel}>To</label>
-                <div className={formStyles.inputFieldWithSymbol}>
-                  <DatePicker
-                    className={formStyles.inputField}
-                    selected={this.state.dateTo}
-                    onChange={this._handleOnChangeDateTo.bind(this)}
-                    maxDate={moment()}
-                    placeholderText="Click here to select a date" />
-                  <div className={formStyles.inputFieldSymbol}>
-                    <i className="fa fa-fw fa-calendar" />
+                <div className={formStyles.formGroup}>
+                  <label className={formStyles.formLabel}>To</label>
+                  <div className={formStyles.inputFieldWithSymbol}>
+                    <DateField
+                      ref="inputDateTo"
+                      clearIcon={false}
+                      renderInput={props => {
+                        props.className = formStyles.inputField;
+                      }}>
+                      <DatePicker
+                        date={dateTo}
+                        maxDate={moment()}
+                        onChange={this._handleOnChangeDateTo.bind(this)}
+                        hideFooter={true}/>
+                    </DateField>
+                    <div className={formStyles.inputFieldSymbol}>
+                      <i className="fa fa-fw fa-calendar" />
+                    </div>
                   </div>
+                  <span className={formStyles.errorMessage}>{this.state.errorMsg}</span>
                 </div>
-                <span className={formStyles.errorMessage}>{this.state.errorMsg}</span>
-              </div>
-            </form>
+              </form>
+            </div>
+            <div className={modalStyles.footer}>
+              <button className={formStyles.submitButton} onClick={this._handleOnClickSubmit.bind(this)}>Add</button>
+              <button className={formStyles.button} onClick={onCloseModal}>Cancel</button>
+            </div>
           </div>
-          <div className={modalStyles.footer}>
-            <button className={formStyles.submitButton} onClick={this._handleOnClickSubmit.bind(this)}>Add</button>
-            <button className={formStyles.button} onClick={onCloseModal}>Cancel</button>
-          </div>
-        </div>
-      </Modal>
+        </Modal>
+      </div>
     );
   }
 
   _handleOnChangeDateFrom(date) {
-    this.setState({ dateFrom: date });
+    const inputDateFrom = jQuery(ReactDOM.findDOMNode(this.refs.inputDateFrom));
+    inputDateFrom.find('input').blur();
+    this.setState({ dateFrom: moment(date) });
   }
 
   _handleOnChangeDateTo(date) {
-    this.setState({ dateTo: date });
+    const inputDateTo = jQuery(ReactDOM.findDOMNode(this.refs.inputDateTo));
+    inputDateTo.find('input').blur();
+
+    const dateTo = moment(date);
+    const dateFrom = moment(this.state.dateFrom);
+
+    if (dateFrom >= dateTo) {
+      this.setState({ dateFrom: dateTo.clone().subtract(1, 'day') });
+    }
+    this.setState({ dateTo: dateTo });
   }
 
   _removeErrors() {
