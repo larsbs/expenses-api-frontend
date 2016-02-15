4902284a34fcaac0e97517bb995dab495c5cc90e
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-15 20:18:54 +0100

    Tests to Spinner component passing

--- test/unit/components/Spinner.jsx
+++ test/unit/components/Spinner.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
 
 import Spinner from '../../../app/components/Spinner';
 
 
+chai.use(spies);
+
+
 describe('@Spinner', function () {
+  let props;
+  let output;
+  let renderer;
+  beforeEach(function () {
+    props = {
+      width: 20,
+      height: 20,
+      borderRadius: 5
+    };
+
+    renderer = TestUtils.createRenderer();
+    renderer.render(<Spinner {...props} />);
+
+    output = renderer.getRenderOutput();
+  });
+  it('should render correctly', function () {
+    expect(output.type).to.equal('div');
+    expect(output.props.children.length).to.equal(9);
+    expect(output.props.style).to.deep.equal({ width: props.width, height: props.height });
+  });
+  it('should assign borderProperty props correctly to first and last children', function () {
+    expect(output.props.children[0].props.style).to.deep.equal({ borderTopLeftRadius: props.borderRadius });
+    expect(output.props.children[8].props.style).to.deep.equal({ borderBottomRightRadius: props.borderRadius });
+  });
 });
